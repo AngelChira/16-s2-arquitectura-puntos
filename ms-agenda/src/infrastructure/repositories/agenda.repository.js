812a8356136365f6651e77a104ce1a1c1852e67c
AngelChira <125518726+AngelChira@users.noreply.github.com>
2025-12-02 00:26:31 -0500

    Para mision 2

--- ms-agenda/src/infrastructure/repositories/agenda.repository.js
+++ ms-agenda/src/infrastructure/repositories/agenda.repository.js
@@ -32,34 +32,41 @@
 // };
 
 // ms-agenda/src/infrastructure/repositories/agenda.repository.js
-const db = require('../../config/db');
+const db = require("../../config/db");
 
 const findBloqueosByTutor = async (idTutor) => {
-    // Seleccionamos también idBloqueo por si acaso
-    const queryText = 'SELECT idBloqueo, idTutor, fechaInicio, duracionMinutos, idEstudiante FROM bloqueos WHERE idTutor = $1 ORDER BY fechaInicio ASC';
-    try {
-        const res = await db.query(queryText, [idTutor]);
-        return res.rows; // Retorna un array de bloqueos
-    } catch (err) {
-        console.error('Error ejecutando query findBloqueosByTutor:', err.stack);
-        throw err;
-    }
+  // Seleccionamos también idBloqueo por si acaso
+  const queryText =
+    "SELECT idbloqueo, idtutor, fechainicio, duracionminutos, idestudiante FROM bloqueos WHERE idtutor = $1 ORDER BY fechainicio ASC";
+  try {
+    const res = await db.query(queryText, [idTutor]);
+    return res.rows; // Retorna un array de bloqueos
+  } catch (err) {
+    console.error("Error ejecutando query findBloqueosByTutor:", err.stack);
+    throw err;
+  }
 };
 
 const saveBloqueo = async (datosBloqueo) => {
-    const { idTutor, fechaInicio, duracionMinutos, idEstudiante } = datosBloqueo;
-    // Quitamos idBloqueo porque lo genera la BD
-    const queryText = 'INSERT INTO bloqueos(idTutor, fechaInicio, duracionMinutos, idEstudiante) VALUES($1, $2, $3, $4) RETURNING *'; // RETURNING * devuelve la fila insertada
-    try {
-        const res = await db.query(queryText, [idTutor, fechaInicio, duracionMinutos, idEstudiante]);
-        return res.rows[0]; // Retorna el bloqueo recién creado con su ID
-    } catch (err) {
-        console.error('Error ejecutando query saveBloqueo:', err.stack);
-        throw err;
-    }
+  const { idTutor, fechaInicio, duracionMinutos, idEstudiante } = datosBloqueo;
+  // Quitamos idBloqueo porque lo genera la BD
+  const queryText =
+    "INSERT INTO bloqueos(idtutor, fechainicio, duracionminutos, idestudiante) VALUES($1, $2, $3, $4) RETURNING *"; // RETURNING * devuelve la fila insertada
+  try {
+    const res = await db.query(queryText, [
+      idTutor,
+      fechaInicio,
+      duracionMinutos,
+      idEstudiante,
+    ]);
+    return res.rows[0]; // Retorna el bloqueo recién creado con su ID
+  } catch (err) {
+    console.error("Error ejecutando query saveBloqueo:", err.stack);
+    throw err;
+  }
 };
 
 module.exports = {
-    findBloqueosByTutor,
-    saveBloqueo
+  findBloqueosByTutor,
+  saveBloqueo,
 };
